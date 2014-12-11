ee907220bf33602c2856908f48dc4b7848652692
Alex Scott <alex.scott@tipsforthings.com>
2014-12-11 23:43:16 +0000

    Foundation Sidebar Rails 4.2.0

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+end
---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class MainControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get privacy" do
+    get :privacy
+    assert_response :success
+  end
+
+  test "should get terms" do
+    get :terms
+    assert_response :success
+  end
+
+end
